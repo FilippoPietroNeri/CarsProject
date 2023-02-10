b35d948d862549a8287be8056ce7c57cad66e03b
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2023-02-10 12:37:00 +0000

    commit_1

--- src/app/mock-cars.ts
+++ src/app/mock-cars.ts
@@ -1,7 +1,7 @@
 import { Car } from './car';
 
 export const CARS: Car[] = [
-  { id: 11, name: 'Putno', costo:10, consumo:25 },
+  { id: 11, name: 'Punto', costo:10, consumo:25 },
   { id: 12, name: 'Bravo', costo:20, consumo:20 }, 
   { id: 13, name: 'Tipo', costo:15, consumo:10 }, 
   { id: 14, name: '124_spider' , costo:100, consumo:90 },
